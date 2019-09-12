245302dd69a2fa9c3c07dee2697b42668b4c3f9a
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-09-12 13:55:24 -0700

    update config

--- .jenkins/.pipeline/lib/config.js
+++ .jenkins/.pipeline/lib/config.js
@@ -6,9 +6,9 @@ const name = 'jenkins'
 
 const phases = {
   build: {namespace:'ocp101a-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
-  dev: {namespace:'ocp101a-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
-  test: {namespace:'ocp101a-dev'    , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`  , instance: `${name}-test`  , version:`${version}`, tag:`test-${version}`},
-  prod: {namespace:'ocp101a-dev'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
+  dev: {namespace:'ocp101a-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-tools-${changeId}`  , instance: `${name}-tools-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
+  test: {namespace:'ocp101a-tools'    , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`  , instance: `${name}-test`  , version:`${version}`, tag:`test-${version}`},
+  prod: {namespace:'ocp101a-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
 };
 
 // This callback forces the node process to exit as failure.
